76d4239816ab4019680d5134ffdfdc8ef930758c
Guilherme de Almeida Pereira <90074528+GuilhermeDeAlmeidaPereira@users.noreply.github.com>
2024-05-26 14:58:29 -0300

    Revert "Firts Commit"
    This reverts commit cb5012fb5aed35a7c103df396fa09e918e956768.

--- Views/Clientes/retorno.php
+++
@@ -1,4 +0,0 @@
-<?php
-include_once '../../config.php';
-
-header("location:". SITE_URL .  "/Views/Clientes/loginCliente.php");
--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-include_once './config.php';
-
-// direcionar para home de vendas
-header("location:" . SITE_URL . "/Views/home/index.php");
